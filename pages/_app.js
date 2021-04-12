1b8a6c05e8ea65b48beece604896b4a648e3b86f
Arnar Arnarson <arnararnarson@gmail.com>
2021-04-12 17:45:43 +0000

    Tailwind added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { ApolloProvider } from '@apollo/client';
 import { useApollo } from '../lib/apolloClient';
+import 'tailwindcss/tailwind.css';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
