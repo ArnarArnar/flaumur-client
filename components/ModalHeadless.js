63d383a526575c8e2b081ab736698e53af1561b7
Arnar Arnarson <arnararnarson@gmail.com>
2021-05-12 15:12:29 +0000

    Fixing opacity fade in/fade out

--- components/ModalHeadless.js
+++ components/ModalHeadless.js
@@ -27,14 +27,13 @@ export default function ModalHeadless({ data, open }) {
             <Dialog as="div" className="fixed inset-0 z-10 overflow-y-auto" onClose={closeModal}>
                 <div className="min-h-screen px-4 text-center">
                     <Transition.Child
-                        as={Fragment}
-                        enter="ease-out duration-300"
+                        enter="transition-opacity ease-linear duration-300"
                         enterFrom="opacity-0"
                         enterTo="opacity-100"
-                        leave="ease-in duration-200"
+                        leave="transition-opacity ease-linear duration-200"
                         leaveFrom="opacity-100"
                         leaveTo="opacity-0">
-                        <Dialog.Overlay className="fixed inset-0" />
+                        <Dialog.Overlay className="fixed inset-0 bg-black opacity-20" />
                     </Transition.Child>
 
                     {/* This element is to trick the browser into centering the modal contents. */}
